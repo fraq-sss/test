9e6fce51e88908366fd8d275de15d21cc64d0559
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2024-12-21 10:26:22 -0800

    removing json import test.py

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import requests
-import json
 
 webhook_url = "https://discord.com/api/webhooks/1314158911267536936/k3Q9W4xp5L1kLTOwGMXAzlwQepVScbrt60hamaK50ivfswiRgHhieJreY1pNWCRJ-hew"
 message_payload = {"content": "Hello :imp:"}
-response = requests.post(webhook_url, data=json.dumps(message_payload), headers={'Content-Type': 'application/json'})
+response = requests.post(webhook_url, json=message_payload, headers={'Content-Type': 'application/json'})
