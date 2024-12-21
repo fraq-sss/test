7af04bc32bcfb9da63588ad0caab1c412851ee01
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2024-12-21 15:22:19 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
+
 import json
 import urllib.request
-#import ba
+import urllib.parse
 
-u = "https://discord.com/api/webhooks/1314158911267536936/k3Q9W4xp5L1kLTOwGMXAzlwQepVScbrt60hamaK50ivfswiRgHhieJreY1pNWCRJ-hew"
-d = json.dumps({"content": "Hello, Discord!"}).encode("utf-8")
-r = urllib.request.Request(u, data=d, headers={"Content-Type": "application/json"})
-with urllib.request.urlopen(r) as s:
-  pass
+webhook_url = "https://discord.com/api/webhooks/1314158911267536936/k3Q9W4xp5L1kLTOwGMXAzlwQepVScbrt60hamaK50ivfswiRgHhieJreY1pNWCRJ-hew"  # Replace with your webhook URL
+message = "Hello from Python!"
+
+data = {
+    "content": message
+}
+
+# Encode the data as JSON
+data = json.dumps(data).encode("utf-8")
+
+# Create the request
+req = urllib.request.Request(webhook_url, data=data, headers={
+    "Content-Type": "application/json"
+})
+
+# Send the request
+response = urllib.request.urlopen(req)
+
+print(response.read().decode("utf-8"))
