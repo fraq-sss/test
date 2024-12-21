34b7a85a0d7ba8253198f28efd8e847daabdf061
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2024-12-21 15:05:31 -0800

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import urllib
 #import ba
 
 u = "https://discord.com/api/webhooks/1314158911267536936/k3Q9W4xp5L1kLTOwGMXAzlwQepVScbrt60hamaK50ivfswiRgHhieJreY1pNWCRJ-hew"
-d = json.dumps({"content": "Hello, Discord!"}).encode("utf-8")
+d = json.dumps({"content": "Hello, Discord!"})#.encode("utf-8")
 r = urllib.request.Request(u, data=d, headers={"Content-Type": "application/json"})
 with urllib.request.urlopen(r):
   pass
