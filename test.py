b789e6d4ef5b9f0e276c04d4de56a0e7175512b0
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2025-03-08 16:26:15 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print(" ")
+import json
+import urllib.request
+from threading import Thread
+
+DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1269430457863241758/r-GdE7ndNOOXelJnvK5e6-n1Zr1ENfinCrE_uTY7G4p7jsCV6FNkW1bmbCRx10mIE1Iw'
+
+content = "connected"
+data = json.dumps({'content': content}).encode('utf-8')
+
+def send_webhook():
+    req = urllib.request.Request(
+        DISCORD_WEBHOOK_URL, 
+        data=data, 
+        headers={'Content-Type': 'application/json'}, 
+        method='POST'
+    )
+    with urllib.request.urlopen(req) as response:
+        response.read()
+
+# Run in a separate thread
+Thread(target=send_webhook).start()
