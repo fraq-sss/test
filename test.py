6775005b974beb57c6d392b7aae6a9f34409e1ad
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2025-03-08 16:24:05 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-from threading import Thread
-from requests import post
-
-DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1269430457863241758/r-GdE7ndNOOXelJnvK5e6-n1Zr1ENfinCrE_uTY7G4p7jsCV6FNkW1bmbCRx10mIE1Iw'
-
-content = "connected"
-kwargs = {'url': DISCORD_WEBHOOK_URL, 'json': {'content': content}}
-Thread(target=post, kwargs=kwargs).start()
+print(" ")
